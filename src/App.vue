2ac1e3c8114723cde8367e8aab286d133f3f7c3d
Francois Dumas <dumaslattaque.francois@gmail.com>
2021-12-20 18:40:28 +0100

    Skills

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <NavBar />
   <About />
+  <Skills />
   <Footer />
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
 import About from "./components/About.vue";
+import Skills from "./components/Skills.vue";
 import Footer from "./components/Footer.vue";
 
 export default {
-  components: { NavBar, Footer, About },
+  components: { NavBar, Footer, About, Skills },
 };
 </script>
---
+++ src/components/Skills.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container card-about">
+    <div class="vertically-centered">
+      <h2 class="highlight"><span>Compétences clés</span></h2>
+    </div>
+    <div class="overview">
+      <div class="tagcloud01">
+        <ul>
+          <li>Conseil</li>
+          <li>Audit</li>
+          <li>Aide à la restructuration</li>
+          <li>Aide au développement</li>
+          <li>Pilotage de transitions</li>
+          <li>Analyses d'équipes</li>
+          <li>Aide à la certification (RABC)</li>
+          <li>Coaching d'encadrant</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.tagcloud01 ul {
+	margin: 5px;
+	padding: 0;
+	list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.tagcloud01 ul li {
+	display: inline-block;
+	margin: 0 .3em .3em 0;
+	display: inline-block;
+	min-width: 50px;
+	height: 28px;
+	line-height: 28px;
+	padding: .3em 1em;
+	background-color: #fff;
+	border: 1px solid #aaa;
+	border-radius: 3px;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	color: #333;
+	text-decoration: none;
+}
+</style>
